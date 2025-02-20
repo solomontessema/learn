31ad84e68e1de2599c4d355e3cfa0ff538b3fbea
solomon-tessema <solomon.tessema@outlook.com>
2025-02-20 08:56:42 -0600

    changed the app.py and test.html from hello world to adding number application

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route('/add', methods=['POST'])
+def add_numbers():
+    data = request.get_json()
+    num1 = data.get('num1')
+    num2 = data.get('num2')
+    result = num1 + num2
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
